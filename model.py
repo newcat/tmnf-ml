439300aa9fbbfa8656a9598cd735de50532c6604
newcat <freddy.wagner@web.de>
2020-06-16 01:36:01 +0200

    Trying to train, needs work

--- model.py
+++ model.py
@@ -1,25 +1,37 @@
-from imagegrab import get_tm_hwnd, get_img
 import presskey as pk
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.optimizers import SGD
+from sklearn.model_selection import train_test_split
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.metrics import confusion_matrix, classification_report
 import numpy as np
-from scipy.special import softmax
-import cv2
-import time
-import random
+from os import listdir
+from os.path import isfile, join
+
+config = tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.8))
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(session)
 
 WIDTH = 200
 HEIGHT = 150
-EXPLORATION_RATE = 0.1
 
 ACTIONS = [
     [],
+    [pk.LEFT_ARROW],
+    [pk.RIGHT_ARROW],
     [pk.UP_ARROW],
     [pk.UP_ARROW, pk.LEFT_ARROW],
     [pk.UP_ARROW, pk.RIGHT_ARROW],
     [pk.DOWN_ARROW],
     [pk.DOWN_ARROW, pk.LEFT_ARROW],
-    [pk.DOWN_ARROW, pk.RIGHT_ARROW]
+    [pk.DOWN_ARROW, pk.RIGHT_ARROW],
+    [pk.UP_ARROW, pk.DOWN_ARROW],
+    [pk.UP_ARROW, pk.DOWN_ARROW, pk.LEFT_ARROW],
+    [pk.UP_ARROW, pk.DOWN_ARROW, pk.RIGHT_ARROW]
 ]
 
 model = Sequential()
@@ -29,33 +41,43 @@ model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
 model.add(Dense(64, activation='relu'))
-model.add(Dense(7))
+model.add(Dense(len(ACTIONS), activation="sigmoid"))
+
+
+def load_training_data():
+    training_files = [f for f in listdir("training") if isfile(join("training", f))]
+    X = None
+    y = None
+    for f in training_files:
+        a = np.load(join("training", f))
+        X = np.concatenate((X, a["images"]), axis=0) if X is not None else a["images"]
+        y = np.concatenate((y, a["actions"]), axis=0) if y is not None else a["actions"]
+
+    X = X.swapaxes(1, 2)
+    # one-hot encoding for action
+    y = to_categorical(y, num_classes=len(ACTIONS))
+
+    return train_test_split(X, y, test_size=0.2)
 
 
 def main():
+    X_train, X_test, y_train, y_test = load_training_data()
 
-    time.sleep(3)
-    hwnd = get_tm_hwnd()
-    current_pressed_keys = []
+    y_integers = np.argmax(y_train, axis=1)
+    print(np.unique(y_integers))
+    class_weights = compute_class_weight('balanced', classes=list(range(len(ACTIONS))), y=y_integers)
+    d_class_weights = dict(enumerate(class_weights))
+    print(class_weights)
 
-    while True:
-        img = get_img(hwnd)
-        img = cv2.resize(img, (WIDTH, HEIGHT))
-        img = img.reshape(1, WIDTH, HEIGHT, 3)
-        prediction = softmax(model.predict(img)[0])
-        action = np.argmax(prediction)
+    opt = SGD(lr=1)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, class_weight=d_class_weights)
 
-        if random.random() < EXPLORATION_RATE:
-            action = random.randrange(0, len(ACTIONS))
-
-        for key in current_pressed_keys:
-            pk.ReleaseKey(key)
-
-        current_pressed_keys = ACTIONS[action]
-        for key in current_pressed_keys:
-            pk.PressKey(key)
-
-        print(action)
+    y_pred = model.predict(X_test)
+    y_pred = np.argmax(y_pred, axis=1)
+    y_true = np.argmax(y_test, axis=1)
+    print(classification_report(y_true, y_pred))
+    print(confusion_matrix(y_true, y_pred))
 
 
 main()
